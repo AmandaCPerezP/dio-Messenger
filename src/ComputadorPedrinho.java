94775aac7a46927368f11a978ce57dca0ef3e79c
Amanda Caroline Perez Pinto <amandacperezp@gmail.com>
2025-02-03 10:36:52 -0300

    Elucidando de forma simples a Herança em POO.

--- src/ComputadorPedrinho.java
+++ src/ComputadorPedrinho.java
@@ -1,12 +1,20 @@
 
 public class ComputadorPedrinho {
 	public static void main (String[] args) {
+		System.out.println("MSN");
 		MSNMensager msn = new MSNMensager();
-		
-		
 		msn.enviarMensagem();
-		
 		msn.receberMensagem();
+		
+		System.out.println("Facebook");
+		FacebookMensager fcb = new FacebookMensager();
+		fcb.enviarMensagem();
+		fcb.receberMensagem();
+		
+		System.out.println("Telegram");
+		Telegram tlg = new Telegram();
+		tlg.enviarMensagem();
+		tlg.receberMensagem();
 	}
 
 }
---
+++ src/FacebookMensager.java
@@ -0,0 +1,4 @@
+
+public class FacebookMensager extends ServicoMensagemInstantanea{
+	
+}
