a1eccfbf384a07e7c166cd617b8af7accc4454ad
Amanda Caroline Perez Pinto <amandacperezp@gmail.com>
2025-02-03 10:56:53 -0300

    Mostrando como funciona a classe Abstract em JAVA.

--- src/FacebookMensager.java
+++ src/FacebookMensager.java
@@ -1,4 +1,19 @@
 
 public class FacebookMensager extends ServicoMensagemInstantanea{
+
+	@Override
+	public void enviarMensagem() {
+		// TODO Auto-generated method stub
+		System.out.println("Enviando mensagem pelo Facebook.");
+		
+	}
+
+	@Override
+	public void receberMensagem() {
+		// TODO Auto-generated method stub
+		System.out.println("Recebendo mensagem pelo Facebook.");
+		
+	}
+	
 	
 }
--- src/MSNMensager.java
+++ src/MSNMensager.java
@@ -1,4 +1,18 @@
 
 public class MSNMensager extends ServicoMensagemInstantanea{
+
+	@Override
+	public void enviarMensagem() {
+		// TODO Auto-generated method stub
+		System.out.println("Enviando mensagem pelo MSN.");
+		
+	}
+
+	@Override
+	public void receberMensagem() {
+		// TODO Auto-generated method stub
+		System.out.println("Recebendo mensagem pelo MSN.");
+		
+	}
 	
 }
